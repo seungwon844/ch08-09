b74271d1b63daa12ce73536f220438000a430e8d
seungwon844 <qw84@naver.com>
2018-10-15 15:13:26 +0900

    Changed ...

---
+++ bookmark/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^bookmark/$', views.BookmarkLV.as_view(), name='index'),
+    url(r'^bookmark/(?P<pk>\d+)/$', views.BookmarkDV.as_view(), name='detail'),
+    url(r'^bookmark_t_CBV/$', views.BookmarkLV.as_view(), name='index_t_CBV'),
+    url(r'^bookmark_t_FBV/$', views.tabularBookmark, name='index_t_FBV'),
+]
